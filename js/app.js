31fb178a95ca73c9d8897f759a930ba2f88893df
alamin0561464977 <amin23435678901315@gmail.com>
2022-03-15 01:38:25 +0300

    Project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('Trigger Button')
+const btn = document.getElementById("submitButton");
+const inputField = document.getElementById("input-field");
+
+inputField.addEventListener("keypress", function (event) {
+    if (event.key == 'Enter')
+        btn.click();
+});
+
+btn.addEventListener('click', function () {
+    const inputValue = inputField.value;
+    inputField.value = '';
+    if (inputValue === 'alamin') {
+        const section = document.getElementById('section');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="search-field">
+            <img src="images/alamin.jpg" alt="alamin.jpg">
+        </div>
+    `;
+        section.appendChild(div)
+    }
+});
